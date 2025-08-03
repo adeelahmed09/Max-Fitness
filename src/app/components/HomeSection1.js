8306d756bb22dea97140cc2602e66a0e4ad8f16d
adeelahmed9 <adeel0ahmed10@gmail.com>
2025-08-03 08:45:08 +0500

    Add GSAP animations and update dependencies in HomeSection1 component

--- src/app/components/HomeSection1.js
+++ src/app/components/HomeSection1.js
@@ -1,25 +1,44 @@
 "use client"
-import React from 'react'
-import { Zen_Dots,Space_Grotesk } from "next/font/google"
+import React, { useEffect } from 'react'
+import { useRef } from 'react';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { Zen_Dots, Space_Grotesk } from "next/font/google"
 const zen_dots = Zen_Dots({
-  subsets: ["latin"],
-  weight: ["400"],     
+    subsets: ["latin"],
+    weight: ["400"],
 });
 const space = Space_Grotesk({
-  subsets: ["latin"],
-  weight: ["400"],     
+    subsets: ["latin"],
+    weight: ["400"],
 });
 function HomeSection1() {
+    gsap.registerPlugin(useGSAP);
+    const mainHeading = useRef(null)
+    const paragraph = useRef(null)
+    useEffect(() => {
+        const tl = gsap.timeline({ defaults: {  opacity: 0, ease: "power2.out" } })
+        tl.from(mainHeading.current, {
+            y: 45,
+            opacity: 0,
+            duration: .5,
+        })
+        tl.from(paragraph.current, {
+            y: 45,
+            opacity: 0,
+            duration: .5,
+        })
+    }, [])
     return (
-        <div className='h-screen w-screen flex justify-center flex-col items-center relative gap-4 overflow-hidden'>
-            <img src="/GymMainHeroImage.webp" className="-z-20 brightness-35 absolute object-center w-full" alt="" />
-            <h1 className={`text-4xl md:text-6xl tracking-tighter text-white uppercase ${zen_dots.className}`}>
+        <div className='h-screen w-[100vw] flex justify-center flex-col items-center relative sm:gap-4 gap-2 overflow-hidden'>
+            <img src="/GymMainHeroImage.webp" className="-z-20 brightness-35 w-full h-full absolute object-cover sm:object-center object-[-500px] w-full" alt="" />
+            <h1 ref={mainHeading} className={`sm:text-4xl text-3xl md:text-6xl tracking-tighter text-center text-white uppercase ${zen_dots.className}`}>
                 Unlock <span className='primaryColor'>Your</span> Power
             </h1>
-            <p className={`${space.className} text-lg md:text-2xl text-white tracking-wide`}>
+            <p ref={paragraph} className={`${space.className}  text-center px-3 sm:px-0 sm:text-2xl text-white tracking-wide`}>
                 Build the Strongest Version of Yourself
             </p>
-            <button className={`px-3 py-2 font-extrabold text-lg rounded-xl secondaryBackColor  ${zen_dots.className}`}>
+            <button className={`px-3 py-2 font-extrabold sm:text-lg rounded-xl secondaryBackColor  ${zen_dots.className}`}>
                 Join Now
             </button>
         </div>
