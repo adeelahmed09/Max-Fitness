dff6f91e3764395b543e367d3ae9cb3a70d1897f
adeelahmed9 <adeel0ahmed10@gmail.com>
2025-08-03 10:15:41 +0500

    Update animations and responsive styles in HomeSection1 and Nav components

--- src/app/components/HomeSection1.js
+++ src/app/components/HomeSection1.js
@@ -17,28 +17,30 @@ function HomeSection1() {
     const mainHeading = useRef(null)
     const paragraph = useRef(null)
     useEffect(() => {
-        const tl = gsap.timeline({ defaults: {  opacity: 0, ease: "power2.out" } })
-        tl.from(mainHeading.current, {
-            y: 45,
-            opacity: 0,
-            duration: 1.5,
-        })
-        tl.from(paragraph.current, {
+        gsap.from(mainHeading.current, {
             y: 45,
             opacity: 0,
             duration: 1,
+            ease: "power2.out"
+        })
+        gsap.from(paragraph.current, {
+            y: 45,
+            opacity: 0,
+            duration: 1,
+            delay:.5,
+            ease: "power2.out"
         })
     }, [])
     return (
-        <div className='h-screen w-[100vw] flex justify-center flex-col items-center relative sm:gap-4 gap-2 overflow-hidden'>
+        <div className='h-screen w-[100vw] flex justify-center flex-col items-center relative lg:gap-4 sm:gap-3 gap-2 overflow-hidden'>
             <img src="/GymMainHeroImage.webp" className="-z-20 brightness-35 w-full h-full absolute object-cover sm:object-center object-[-500px] w-full" alt="" />
-            <h1 ref={mainHeading} className={`sm:text-4xl text-3xl md:text-6xl tracking-tighter text-center text-white uppercase ${zen_dots.className}`}>
+            <h1 ref={mainHeading} className={`sm:text-4xl text-3xl lg:text-6xl tracking-tighter text-center text-white uppercase ${zen_dots.className}`}>
                 Unlock <span className='primaryColor'>Your</span> Power
             </h1>
-            <p ref={paragraph} className={`${space.className}  text-center px-3 sm:px-0 sm:text-2xl text-white tracking-wide`}>
+            <p ref={paragraph} className={`${space.className}  text-center px-3 sm:px-0 sm:text-lg text-white tracking-wide`}>
                 Build the Strongest Version of Yourself
             </p>
-            <button className={`px-3 py-2 font-extrabold sm:text-lg rounded-xl secondaryBackColor  ${zen_dots.className}`}>
+            <button className={`px-3 py-2 font-extrabold sm:text-lg rounded-xl bg-[#CCEE0C] hover:bg-white border-2 border-[#CCEE0C] hover:border-black transition  ${zen_dots.className}`}>
                 Join Now
             </button>
         </div>
--- src/app/components/Nav.js
+++ src/app/components/Nav.js
@@ -14,16 +14,19 @@ function Nav() {
     <>
     <nav className='sm:flex hidden px-16 absolute w-full z-10 justify-between items-center' id='pcNav'>
         <section className='left-nav'>
-            <h1 className={`${zen_dots.className} text-3xl text-white`}>Max <span className='text-[#CCEE0C]'>Fitness</span> Center</h1>
+            <h1 className={`${zen_dots.className} lg:text-3xl sm:text-xl text-white`}>Max <span className='text-[#CCEE0C]'>Fitness</span> Center</h1>
         </section>
         <section className='right-nav'>
-            <ul className={`text-lg ${zen_dots.className} flex items-center gap-5 text-white`}>
-                <li className={`${pathName==="/"?"border-t-2 border-[#CCEE0C] text-[#CCEE0C]":"border-none text-white"} py-7`}><Link href={"/"}>Home</Link></li>
-                <li className={`${pathName==="/about-us"?"border-t-2 border-[#CCEE0C] text-[#CCEE0C]":"border-none  text-white"} py-7`}><Link href={"#"}>About Us</Link></li>
-                <li className={`${pathName==="/contact-us"?"border-t-2 border-[#CCEE0C] text-[#CCEE0C]":"border-none  text-white"} py-7`}><Link href={"#"}>Contact Us</Link></li>
+            <ul className={`lg:text-lg sm:text ${zen_dots.className} flex items-center gap-5 text-white`}>
+                <li className={`${pathName==="/"?"border-t-2 border-[#CCEE0C] text-[#CCEE0C]":"border-none text-white hover:text-[#CCEE0C]"} py-7`}><Link href={"/"}>Home</Link></li>
+                <li className={`${pathName==="/about-us"?"border-t-2 border-[#CCEE0C] text-[#CCEE0C]":"border-none hover:text-[#CCEE0C] text-white"} py-7`}><Link href={"#"}>About Us</Link></li>
+                <li className={`${pathName==="/contact-us"?"border-t-2 border-[#CCEE0C] text-[#CCEE0C]":"border-none hover:text-[#CCEE0C] text-white"} py-7`}><Link href={"#"}>Contact Us</Link></li>
             </ul>
         </section>
     </nav>
+    <div className='absolute top-0 w-full text-center pt-4 sm:hidden'>
+        <h1 className={`${zen_dots.className} text-xl text-white`}>Max <span className='text-[#CCEE0C]'>Fitness</span> Center</h1>
+    </div>
     <nav className='sm:hidden' id='mobileNav'>
 
     </nav>
